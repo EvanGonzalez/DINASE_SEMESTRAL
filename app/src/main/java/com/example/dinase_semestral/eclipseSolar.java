811c0c3034067eb8925cb4969cff0c5b8c3a5c1b
AngelaYermey <carrionangela53@gmail.com>
2022-06-26 10:26:09 -0500

    cambios en menu

---
+++ app/src/main/java/com/example/dinase_semestral/eclipseSolar.java
@@ -0,0 +1,14 @@
+package com.example.dinase_semestral;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class eclipseSolar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eclipse_solar);
+    }
+}
